1283aa0eb90c8c31b2de45370482d5de21c79867
Lakpathi009 <97522196+Lakpathi009@users.noreply.github.com>
2022-02-25 15:16:15 +0530

    Update naik01_s3.tf
    8th commit

--- naik01_s3.tf
+++ naik01_s3.tf
@@ -1,13 +1,16 @@
 resource "aws_s3_bucket" "b" {
     bucket  = "naik01"
+    s3Upload(file:'s3_file.tf', bucket:'naik01', path:'F:\Algorithams_acadamy\templates\s3_file.tf')
+    s3Upload(file:'New folder', bucket:'naik01', path:'F:\Algorithams_acadamy\templates\New folder')
 
     tags    = {
      environment = "Dev" 
     }
 }
 #uploading a file into bucket 
-resource "aws_s3_bucket" "object1" {
-  bucket = "naik01"
-  key    = "Python_Hardway.pdf"
-  source = "F:\\python\\python\\wifi_Hack\\Python_Hardway.pdf"
-}
+#resource "aws_s3_bucket" "object1" {
+ # bucket = "naik01"
+  #key    = "Python_Hardway.pdf"
+  #source = "F:\\python\\python\\wifi_Hack\\Python_Hardway.pdf"
+#}
+
