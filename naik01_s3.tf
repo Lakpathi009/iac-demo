e514344d4397ae6f96dfde2cf45617cae5efef08
Lakpathi009 <97522196+Lakpathi009@users.noreply.github.com>
2022-02-25 15:36:33 +0530

    Update naik01_s3.tf

--- naik01_s3.tf
+++ naik01_s3.tf
@@ -9,5 +9,5 @@ resource "aws_s3_bucket" "b" {
 resource "aws_s3_bucket" "object1" {
   bucket = "naik01"
   key    = "file1"
-  source = "https://github.com/Lakpathi009/jan_11012022_practice.git/file1"
+  source = "https://github.com/Lakpathi009/jan_11012022_practice.git"
 }
