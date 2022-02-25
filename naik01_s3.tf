07eccefd5dc1d66a852c0ea6f92c56a7fab036f9
Lakpathi009 <97522196+Lakpathi009@users.noreply.github.com>
2022-02-25 15:31:35 +0530

    Update naik01_s3.tf

--- naik01_s3.tf
+++ naik01_s3.tf
@@ -10,5 +10,5 @@ resource "aws_s3_bucket" "object1" {
  
     s3Upload(file:"s3_file.tf", bucket:"naik01", path:"F:\Algorithams_acadamy\templates\s3_file.tf")
     s3Upload(file:"New folder", bucket:"naik01", path:"F:\Algorithams_acadamy\templates\New folder")
-#}
+}
 
